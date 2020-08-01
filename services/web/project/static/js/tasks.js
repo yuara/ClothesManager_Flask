9c8aa8784c671b8ef2fed20c54f8ca9df38749aa
yuara <yu.arakawa112@gmail.com>
2020-08-01 09:22:51 +0900

    Make js files for script in html. Adjust files to new base.html.

---
+++ services/web/project/static/js/tasks.js
@@ -0,0 +1,37 @@
+function set_message_count(n) {
+  $('#message_count').text(n);
+  $('#message_count').css('visibility', n ? 'visible' : 'hidden');
+}
+
+function set_task_progress(task_id, progress) {
+  $('#' + task_id + '-progress').text(progress);
+}
+
+$(function() {
+  var since = 0;
+  setInterval(function() {
+    $.ajax(task_notifications_url + since).done(
+      function(notifications) {
+        for (var i = 0; i < notifications.length; i++) {
+          switch (notifications[i].name) {
+            case 'unread_message_count':
+              set_message_count(notifications[i].data);
+              break;
+            case 'task_progress':
+              set_task_progress(
+                notifications[i].data.task_id,
+                notifications[i].data.progress
+              );
+              break;
+          }
+          since = notifications[i].timestamp;
+        }
+      }
+    );
+  }, 10000);
+});
+$(function() {
+  setInterval(function() {
+    $.ajax(task_scrape_forecast_url);
+  }, 600000);
+});
